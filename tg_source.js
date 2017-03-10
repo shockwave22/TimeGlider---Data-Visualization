dd5846657bf29af538c4df3c68d10d9a7ba6356a
Monish Chandrashekar <mc6777@rit.edu>
2017-03-09 19:25:25 -0500

    Add files via upload

---
+++ tg_source.js
@@ -0,0 +1,21 @@
+var tg_data_source = [
+
+  // Advantages of this method include being able to 
+  // load from remote URLs, add comments to the data
+  // and of course, use functions!
+  {
+    "id":"js_history", // a unique identifier
+    "title":"A little history of JavaScript",
+    "description":"Javascript emerged in a specific context...",
+    "focus_date":"1995-06-20 12:00:00",
+    "initial_zoom":"39",
+    "image_lane_height":50,
+    "events":[
+      {
+        "id":"jshist-Lambda",
+        "title": "Lambda Calculus",
+        ...
+      },
+      // more events
+  }
+]; // end of your data source array
